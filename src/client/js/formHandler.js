e066317d0fff7cecceb84d1de3cbc5ffeb379ab8
nazaza <azazanafaa@gmail.com>
2020-04-29 01:12:59 +0200

    #Adding weather api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -10,7 +10,24 @@ function handleSubmit(event) {
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
+    }).then(res => getWeather())
+    .then((data) => {
+        console.log(data);
     })
+}
+
+const apikey = 'ffed4aa63204dd3d8fb822260953b11e';
+const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?';
+
+const getWeather = async () => {
+    const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?zip=${document.getElementById('name').value}&appid=ffed4aa63204dd3d8fb822260953b11e`)
+    try {
+        const res = await response.json();
+        return res;
+    } catch (error) {
+        console.log('error');
+    }
 }
 
 export { handleSubmit }
